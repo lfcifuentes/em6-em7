1af949d74c21b074f999e8fd1354562a9ae8ed74
Luis Fernando Cifuentes <lfcifuentes28@gmail.com>
2020-11-03 22:41:05 -0500

    videos de es7

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+// nace en junio del 2016
+
+let numbers = [1,2,3,7,8,9,5];
+
+if ( numbers.includes(7) ){
+    console.log("The array contain '7'");
+}else{
+    console.log("the array not contain '7'")
+}
+
+// elevar a la potencia 
+let base = 4;
+let exponent = 7;
+let result = base**exponent;
+
+console.log(result);
